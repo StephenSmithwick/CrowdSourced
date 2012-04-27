c0a25a53292349a76b46c56712ca59164de2cd7e
BrunoChauvet <bruno.chauvet@live.fr>
2012-04-28 00:08:02 +1000

    Integration with Google Places and Twitter

--- lib/crowdsourced.rb
+++ lib/crowdsourced.rb
@@ -1,19 +1,37 @@
 require 'rubygems'
 require 'cassandra'
- 
+require 'twitter'
+require 'open-uri'
+require 'json'
+
+# Retrieves and persits Tweeter feeds
+def parse_tweets(store, city, geocode, term)
+  Twitter.search(term, :rpp => 3, :result_type => "recent", :geocode => geocode, :lang => "en").map do |tweet|
+    puts "#{tweet.from_user}: #{tweet.text}"
+    store.insert(:Tweets, city, {"#{tweet.id}" => "#{tweet.text}"});
+  end
+end
+
+
+# Connect to Cassandra
 store = Cassandra.new('CrowdSourced');
 
-store.insert(:Tweets, 'tweet1', { 'author' => 'Bruno'});
-store.insert(:Tweets, 'tweet1', { 'text' => 'That is my first tweet'});
-store.insert(:Tweets, 'tweet1', { 'url' => 'www.google.com.au'});
- 
-# query information
-user = store.get(:Tweets, 'tweet1')
- 
-# show result
+# Get the list of cafes is Sydney from Google Places
+url = 'https://maps.googleapis.com/maps/api/place/search/json?location=-33.8670522,151.1957362&radius=3000&types=cafe&sensor=false&key=AIzaSyC2pDpNpBWnlNYnBUX363XV5Aog4UdOjeg'
+result = open(url) do |file|
+  result = JSON.parse(file.read)
+  result["results"].each do |location|
+    # Get the list of Tweets for this cafe in Sydney
+    puts location["name"]
+    parse_tweets store, 'Sydney', '33.8670522,151.1957362,3km', location["name"]
+  end
+end
+
+# Display store content
+user = store.get(:Tweets, 'Sydney')
 user.each do |field|
   puts field.inspect
 end
- 
-# remove entries
-store.remove(:Tweets, 'tweet1')
+
+# Clear store
+store.remove(:Tweets, 'Sydney')
